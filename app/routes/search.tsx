ee8b2c2777cdb075e17aad9b98b584a0d0eb26ee
Eliav <eliav1234@instantsearchplus.com>
2024-11-27 18:25:37 +0200

    Update search product image

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -111,7 +111,7 @@ function ProductItem({
       {product.featuredImage && (
         <Image
           alt={product.featuredImage.altText || product.title}
-          aspectRatio="1/1"
+          aspectRatio="0.714"
           data={product.featuredImage}
           loading={loading}
           sizes="(min-width: 45em) 400px, 100vw"
